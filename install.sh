beaadf4262d41b049d02dd1d4670b04bd1e2b1a0
Artur Kowalczyk <kowalczyk.artur@gmail.com>
2017-07-28 11:40:20 +0200

    init commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# clean existing docker
+yum remove docker docker-common docker-selinux docker-engine
+
+# install tools
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# add docker repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# install docker ce
+yum makecache fast
+yum install docker-ce
