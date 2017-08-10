903d2c76eef0f9f16b73ff708529c3af3881b40d
Artur Kowalczyk <kowalczyk.artur@gmail.com>
2017-08-10 17:28:01 +0200

    docker-compose install

--- install.sh
+++ install.sh
@@ -17,4 +17,7 @@ yum install -y docker-ce
 systemctl start docker
 systemctl enable docker
 
+# install docker-compose
+curl -fsSL https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+
 docker version
